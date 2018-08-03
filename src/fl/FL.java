7b9fdb93e5048e5e9c977f7d01259f440662343b
srick <srick@DESKTOP-HI8B31S.home>
2018-08-03 23:02:33 +0200

    First commit

---
+++ src/fl/FL.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fl;
+
+/**
+ *
+ * @author srick
+ */
+
+public class FL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Trapezoid trp = new Trapezoid(0, 10, 9, 12);
+    }
+
+}
